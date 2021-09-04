93fbc2f76a189ffb9cc7bd8cbe9da2c6d5c1e3b9
treatme030 <treatme8192@gmail.com>
2021-09-04 10:55:01 +0900

    firestore에서 데이터 받아오기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,30 @@
 import { dbService } from 'fbase';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
     const [sweet, setSweet] = useState('')
+    //firestore에서 받은 데이터 상태관리
+    const [sweets, setSweets] = useState([])
 
+    //firestore에서 도큐먼트 읽어오기
+    const getSweets = async () => {
+        const dbSweets = await dbService.collection("sweets").get()//스냅샷으로 들어옴
+        dbSweets.forEach((document) => 
+            setSweets([document.data(), ...sweets])
+        )
+    }
+    //useEffect에 async-await문을 쓴 함수는 따로 정의하고, 실행만 그 안에서 해야 함 
+    useEffect(() => {
+        getSweets()
+    },[])
+    console.log(sweets)
+    
     const onChange = (e) => {
         const { value } = e.target
         setSweet(value)
     }
 
+    //firestore에 데이터 저장하기
     const onSubmit = async (e) => {
         e.preventDefault()
         //컬렉션을 생성하고, 해당 컬렉션에 도큐먼트 추가 
