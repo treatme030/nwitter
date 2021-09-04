cd7aa37ff01b1ed92d639abc51436c8f97fca0c1
treatme030 <treatme8192@gmail.com>
2021-09-04 10:25:24 +0900

    firestore에 데이터 저장하기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import { dbService } from 'fbase';
+import React, { useState } from 'react';
 
 const Home = () => {
+    const [sweet, setSweet] = useState('')
+
+    const onChange = (e) => {
+        const { value } = e.target
+        setSweet(value)
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        //컬렉션을 생성하고, 해당 컬렉션에 도큐먼트 추가 
+        await dbService.collection("sweets").add({
+            text: sweet,
+            createdAt: Date.now(),
+        })
+        setSweet('')
+    }
     return (
-        <span>
-            Home
-        </span>
+        <form onSubmit={onSubmit}>
+            <input 
+            type="text" 
+            placeholder="What's on your mind?" 
+            maxLength={120}
+            value={sweet}
+            onChange={onChange}
+            />
+            <input type="submit" value="Sweet"/>
+        </form>
     );
 };
 
