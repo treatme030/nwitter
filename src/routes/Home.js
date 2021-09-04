ce5b1b301a5ad016b7e07328e73bb5c95f769320
treatme030 <treatme8192@gmail.com>
2021-09-04 23:04:28 +0900

    이미지 웹브라우저에 출력 해보기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -34,6 +34,23 @@ const Home = ({ userObj }) => {
         })
         setSweet('')
     }
+
+    //웹 브라우저에 사진 출력하기 
+    const onFileChange = (e) => {
+        const { files } = e.target 
+        const theFile = files[0] // files는 배열형태 
+        const reader = new FileReader()
+        //onloadend: readAsDataURL함수로 전달할 인자(파일)가 함수로 들어간 이후
+        //결괏값이 나온 다음 상황 감지하고, 그때 생긴 이벤트값을 사용 가능하게 해줌 
+        reader.onloadend = (finishedEvent) => {
+            console.log(finishedEvent)
+        }
+        //readAsDataURL함수: 파일 정보를 인자로 받아서 파일 위치를 URL로 반환
+        //웹 브라우저가 파일을 인식하는 시점, 파일 인식이 끝난 시점까지 함께 관리해줘야
+        //URL을 얻을 수 있음 
+        reader.readAsDataURL(theFile)
+    }
+
     return (
         <>
             <form onSubmit={onSubmit}>
@@ -44,6 +61,7 @@ const Home = ({ userObj }) => {
                 value={sweet}
                 onChange={onChange}
                 />
+                <input type="file" accept="image/*" onChange={onFileChange}/>
                 <input type="submit" value="Sweet"/>
             </form>
             <div>
