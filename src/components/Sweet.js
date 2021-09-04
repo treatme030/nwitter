6fc281dd1d6e2ff0d069a4d2bd53fd4a7d86df79
treatme030 <treatme8192@gmail.com>
2021-09-04 12:33:19 +0900

    sweet 삭제 기능 구현

--- src/components/Sweet.js
+++ src/components/Sweet.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { dbService } from 'fbase';
 
-const Sweet = ({ text, isOwner }) => {
+const Sweet = ({ id, text, isOwner }) => {
+    
+    const onDeleteClick = async () => {
+        const ok = window.confirm('삭제하시겠습니까?')//boolean 값 반환 
+        if(ok){
+            const data = await dbService.doc(`sweets/${id}`).delete()
+        }
+    }
+
     return (
         <div>
             <h4>{text}</h4>
             {/* 유저와 작성자가 동일한 경우에만 버튼 보이도록 */}
             { isOwner && (
                 <>
-                    <button>Delete Sweet</button>
+                    <button onClick={onDeleteClick}>Delete Sweet</button>
                     <button>Edit Sweet</button>
                 </>
             )}
