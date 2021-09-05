f11bddf570bdab19daca368940a0d7c47d8de0e7
treatme030 <treatme8192@gmail.com>
2021-09-05 17:00:08 +0900

    GlobalStyle 적용

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
+import GlobalStyles from 'styles/GlobalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
