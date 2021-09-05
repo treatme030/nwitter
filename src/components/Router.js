641759907de204b80d0af56aa3b9db25603170c2
treatme030 <treatme8192@gmail.com>
2021-09-05 10:49:46 +0900

    profile 링크에 사용자 이름 넣기

--- src/components/Router.js
+++ src/components/Router.js
@@ -9,7 +9,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
 
     return (
         <Router>
-            { isLoggedIn && <Navigation/> }
+            { isLoggedIn && <Navigation userObj={userObj}/> }
             <Switch>
                 { isLoggedIn ? (
                     <>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -12,33 +12,9 @@ const Profile = ({ userObj }) => {
         history.push('/') //로그아웃시 처음 화면으로 이동
     }
 
-    //유저가 작성한 sweets만 필터링 하기 
-    const getMySweets = async () => {
-        const sweets = await dbService
-        .collection("sweets")
-        .where("creatorId", "==", userObj.uid)
-        .orderBy("createdAt", "asc")
-        .get()//쿼리문 통해 얻은 결과물 가져오기 
-
-        const userSweetsList = sweets.docs.map((doc) => doc.data())
-        setUserSweets(userSweetsList)
-    }
-
-    useEffect(() => {
-        getMySweets()
-    },[])
 
     return (
         <>
-            { userSweets.map((userSweet, index) => {
-                const { attachmentUrl, createdAt, creatorId, text} = userSweet
-                return (
-                    <div key={index}>
-                        <h4>{text}</h4>
-                        <img src={attachmentUrl} width="50px" height="50px"/>
-                    </div>
-                )
-            })}
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     );
