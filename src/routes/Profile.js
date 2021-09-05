44a11ba8b00b1cf0d9b252a830e450f3f7562749
treatme030 <treatme8192@gmail.com>
2021-09-05 13:03:02 +0900

    코드 수정

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { authService, dbService } from 'fbase';
+import React, { useState } from 'react';
+import { authService } from 'fbase';
 import { useHistory } from 'react-router-dom';
 
 const Profile = ({ userObj, refreshUser }) => {
