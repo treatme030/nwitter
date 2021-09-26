bce88a266ee80c08f813b8dabdc08517343d518d
treatme030 <treatme8192@gmail.com>
2021-09-26 20:20:33 +0900

    공통 스타일 분리

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -37,6 +37,28 @@ const  GlobalStyles = createGlobalStyle`
     form {
         width: 100%;
     }
+    /* common styles */
+    .blue_border_input {
+        height: 40px;
+        padding: 0 2rem;
+        color: #fff;
+        border: 1px solid #04aaff;
+        border-radius: 20px;
+        font-weight: 500;
+        font-size: 1.2rem;
+    }
+    .blue_button {
+        position: absolute;
+        right: 0;
+        background-color: #04aaff;
+        height: 40px;
+        width: 40px;
+        padding: 1rem 0;
+        text-align: center;
+        border-radius: 20px;
+        color: #fff;
+        cursor: pointer;
+    }
 `;
 
 export default GlobalStyles;
